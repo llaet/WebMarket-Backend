b90b19e6fcc90e90ea2f356fa17a8e4a7b95cb67
Lucas Laet <lucas.laetlira@gmail.com>
2021-01-09 18:18:26 -0300

    Transact read only prop added to city query

--- src/main/java/com/saleshub/repositories/CityRepository.java
+++ src/main/java/com/saleshub/repositories/CityRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.saleshub.domain.City;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Integer>{
 
+    @Transactional(readOnly = true)
     List<City> findByStateId(Integer stateId);
 }
